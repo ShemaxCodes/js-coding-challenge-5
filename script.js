07bc224f5729cf7fe3ce97eb44787cab720e83e4
Jashema Panapa <53614792+ShemaxCodes@users.noreply.github.com>
2021-11-26 11:29:48 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
 
+// Create an arrow function 'calcAverage' to calculate the average of 3 scores
+
+
 const calcAverage = (a, b, c) => (a + b + c) / 3; 
-console.log(calcAverage(44, 23, 71));
+console.log(calcAverage(6, 12, 21));
+
+// TEST 1
+
+const scoreDolphins = calcAverage(44, 23, 71);
+const scoreKoalas = calcAverage(65, 54, 49);
+console.log(scoreDolphins, scoreKoalas);
+
+const checkWinner = function(avgDolphins, avgKoalas) {
+    if (avgDolphins >= 2 * avgKoalas) {
+        console.log(`Dolphins win 🏆 (${avgDolphins} vs. ${avgKoalas})`);
+    } else if (addEventListener >= 2 * avgDolphins) {
+        console.log(`Koalas win 🏆 (${avgKoalas} vs. ${avgDolphins})`);
+    } else {
+        console.log("No team wins...")
+    }
+} 
+checkWinner(scoreDolphins, scoreKoalas);
